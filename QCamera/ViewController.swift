73bb419112a9e47f483b80a2ffa326ffb5199f75
leonardo15043 <lahernandez15043@gmail.com>
2018-11-26 23:24:55 -0500

    first commit

--- QCamera/ViewController.swift
+++ QCamera/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate{
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Titulo alerta", message: "Subtitulo alerta", preferredStyle: .actionSheet)
+        
+        let cameraAlert = UIAlertAction(title: "Tomar foto", style: .default) { (action) in
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+        
+        let fotoAlert = UIAlertAction(title: "Buscar foto", style: .default) { (action) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+        
+        let cancelAlert = UIAlertAction(title: "Cancelar", style: .default)
+        
+        actionSheet.addAction(cameraAlert)
+        actionSheet.addAction(fotoAlert)
+        actionSheet.addAction(cancelAlert)
+        
+        present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 

6cb98daf124ae84bbe6145968e3278a11cc00dd5
leonardo15043 <lahernandez15043@gmail.com>
2018-11-26 20:18:20 -0500

    Initial Commit

---
+++ QCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QCamera
+//
+//  Created by leonardo hernandez on 11/26/18.
+//  Copyright © 2018 leonardo hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
